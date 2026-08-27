2ad1c76f6a6da9cef422a80cfc4711bf623e3bfd
Mariusz Jakoniuk <jarmuszz@fixpoint.dev>
2026-08-27 09:19:40 +0200

    Add rebuild argument handling

--- rebuild.scala
+++ rebuild.scala
@@ -49,27 +49,25 @@ object Main extends IOApp {
     go((List.empty, List.empty), node).lst
   }
 
-  def republish(node: String, publishCommand: String => String): IO[Unit] = {
+  def republish(node: String, publishCommand: String => String, ver: ScalaVer): IO[Unit] = {
     val sorted = if (node == rootElement) topsort(node).tail else topsort(node)
-
     Stream
       .emits[IO, String](sorted)
       .evalMap { node => 
-        ProcessBuilder("sbt", publishCommand(node))
-          .withWorkingDirectory(Path(repoPath) / Path(node))
-          .spawn[IO]
-          .use { proc =>
-            val printStdout = proc.stdout.map(_.toChar.toString).evalTapChunk(IO.print).compile.drain
-            val exit = proc.exitValue.ensure(java.io.IOException("Exit code non-zero"))(_ == 0)
-            printStdout >> exit
-          }
+         republishOne(node, publishCommand, ver)
       }
       .compile
       .drain
   }
 
-  def republishOne(node: String, publishCommand: String => String): IO[Unit] = {
-    ProcessBuilder("sbt", publishCommand(node))
+  def republishOne(node: String, publishCommand: String => String, ver: ScalaVer): IO[Unit] = {
+    val action = publishCommand(node)
+    val cmd = "update; " + (ver match {
+      case ScalaVer.all => s"++3.3; $action; ++2.13; $action; ++2.12; $action"
+      case v => s"++$v ; $action"
+    })
+    println(cmd)
+    ProcessBuilder("sbt", cmd)
       .withWorkingDirectory(Path(repoPath) / Path(node))
       .spawn[IO]
       .use { proc =>
@@ -79,16 +77,78 @@ object Main extends IOApp {
       }.void
   }
 
+  sealed trait ScalaVer
+  object ScalaVer {
+    object all extends ScalaVer
+    object _3_3 extends ScalaVer { override def toString() = "3.3" }
+    object _2_13 extends ScalaVer { override def toString() = "2.13" }
+    object _2_12 extends ScalaVer { override def toString() = "2.12" }
+  }
+  
+  enum PublishScope {
+    case All, One
+  }
+
+  enum BuildTarget {
+    case All, JS
+  }
+
+  enum ArgOpt {
+    case ScalaVersion(ver: ScalaVer)
+    case Scope(s: PublishScope)
+    case Target(s: BuildTarget)
+  }
+
+  def parseArgs(args: List[String]): (List[ArgOpt], List[String]) = {
+    @scala.annotation.tailrec
+    def go(acc: List[ArgOpt], left: List[String]): (List[ArgOpt], List[String]) =
+      if (left.isEmpty) (acc, left)
+      else {
+        val pair = left match {
+          case "-V" :: "2.12" :: tail => Option(ArgOpt.ScalaVersion(ScalaVer._2_12) -> tail)
+          case "-V" :: "2.13" :: tail => Option(ArgOpt.ScalaVersion(ScalaVer._2_13) -> tail)
+          case "-V" :: "3.3" :: tail  => Option(ArgOpt.ScalaVersion(ScalaVer._3_3) -> tail)
+          case "-V" :: "all" :: tail  => Option(ArgOpt.ScalaVersion(ScalaVer.all) -> tail)
+          case "-s" :: "all" :: tail  => Option(ArgOpt.Scope(PublishScope.All) -> tail)
+          case "-s" :: "one" :: tail  => Option(ArgOpt.Scope(PublishScope.One) -> tail)
+          case "-t" :: "all" :: tail  => Option(ArgOpt.Target(BuildTarget.All) -> tail)
+          case "-t" :: "js" :: tail   => Option(ArgOpt.Target(BuildTarget.JS) -> tail)
+          case _ => None
+        }
+
+        pair match {
+          case Some(opt, tail) => go(acc :+ opt, tail)
+          case None => (acc, left)
+        }
+      }
+
+    go(List.empty, args)
+  }
 
   def run(args: List[String]): IO[ExitCode] = {
     args match {
-      case List("publishRootAll") => republish("root", config(_).republish).as(ExitCode.Success)
-      case List("publishJS", pkg) => republish(pkg, config(_).republishJS).as(ExitCode.Success)
-      case List("publishAll", pkg) => republish(pkg, config(_).republish).as(ExitCode.Success)
-      case List("publishOne", pkg) => republishOne(pkg, config(_).republish).as(ExitCode.Success)
-      case List("publishOneJS", pkg) => republishOne(pkg, config(_).republishJS).as(ExitCode.Success)
-      case List("list", pkg) => IO.println(topsort(pkg)).as(ExitCode.Success)
-      case _ => IO.raiseError(IllegalArgumentException(s"Invalid args: ${args}"))
+      case "publish" :: tail =>
+        val (opts, rest) = parseArgs(tail)
+        rest.headOption.liftTo[IO] {
+          new IllegalArgumentException("You need to specify package name to publish")
+        }.flatMap { pkg =>
+          val fun = if (opts.contains(ArgOpt.Scope(PublishScope.One))) republishOne else republish
+          val transform = (pkg: String) =>
+            if (opts.contains(ArgOpt.Target(BuildTarget.JS)))
+              config(pkg).republishJS
+            else
+              config(pkg).republish
+
+          val ver = opts.collectFirst {
+            case ArgOpt.ScalaVersion(v) => v
+          }.getOrElse(ScalaVer.all)
+          println(ver)
+
+          fun(pkg, transform, ver).as(ExitCode.Success)
+        }
+      case List("list", pkg) =>
+        IO.println(topsort(pkg)).as(ExitCode.Success)
+      case _ => IO.println("Invalid argument list").as(ExitCode.Error)
     }
   }
 }
